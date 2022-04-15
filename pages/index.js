f2ceb674b2c6617ac3098cfdc72339c1dc59b9d6
kimgiftww <kimgiftww@gmail.com>
2022-04-15 08:53:10 +0000

    width01

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Index({ posts, globalData }) {
             <Header name={globalData.name} />
             <main className="w-full">
                 <h1 className="text-3xl lg:text-5xl text-center mb-12">{globalData.blogTitle}</h1>
-                <div className="grid md:grid-cols-5 lg:-mx-24 mt-12">
+                <div className="grid md:grid-cols-5">
                     {posts.map((post) => (
                         <div
                             key={post.filePath}
